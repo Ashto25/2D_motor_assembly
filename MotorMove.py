029e3073eb7086d1680f186b44ac9d9efc722eaa
Ashton S <ashtonbstroud@gmail.com>
2023-09-18 18:58:31 -0500

    Small Changes

--- MotorMove.py
+++ MotorMove.py
@@ -12,10 +12,13 @@ CCW = 0
 SPR = 200
 
 GPIO.setmode(GPIO.BCM)
+# Motor Num.1
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
+#Motor Num.2
 GPIO.setup(DIR2, GPIO.OUT)
 GPIO.setup(STEP2, GPIO.OUT)
+#Motor Num1,2 set to clockwise motion
 GPIO.output(DIR, CW)
 GPIO.output(DIR2, CW)
 
@@ -27,11 +30,17 @@ RESOLUTION = {'1':(0,0,0),
               '16':(0,0,1),
               '32':(1,0,1)}
 Selected = '16'
+#Setup microstepping
 GPIO.output(MODE, RESOLUTION[Selected])
 
+#Steps per rotation after microstepping selection
 step_count = SPR * int(Selected)
+#Delay between steps
 delay = 1 / step_count / 5
-for r in range(10):
+
+
+#Move in test motion back and forth
+for r in range(1):
     GPIO.output(DIR, CW)
     GPIO.output(DIR2, CW)
     for x in range(step_count*4):
